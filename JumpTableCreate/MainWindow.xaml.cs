985632ac29acd1f86603dbf4c0f865ff33cbc0d0
Dmitry Pankov <pankowda@mail.ru>
2019-11-28 23:04:51 +0300

    Jump Table Col "S"

--- JumpTableCreate/MainWindow.xaml.cs
+++ JumpTableCreate/MainWindow.xaml.cs
@@ -24,8 +24,10 @@ namespace JumpTableCreate
         public int CountOutputI { get; set; }
         public int CountCycleI { get; set; }
         public int CountJumpI { get; set; }
-
+        // Create Document in FlowViewer
         FlowDocument document = new FlowDocument();
+        // Create Table
+        Table table = new Table();
 
         public MainWindow()
         {
@@ -40,7 +42,8 @@ namespace JumpTableCreate
         {
             ArgToInt();
             CreateText(CountInputI, CountOutputI);
-            JumpTable(CountInputI);
+            TitleJumpTable(CountInputI, CountOutputI,CountCycleI ,CountJumpI, table);
+            CreateTable(CountInputI, CountOutputI, CountCycleI, CountJumpI, table);
             MessageBox.Show("Генерация таблицы переходов и выходов.",
                             "Информация",
                             MessageBoxButton.OK,
@@ -49,23 +52,28 @@ namespace JumpTableCreate
         /// <summary>
         /// Создание таблицы переходов
         /// </summary>
-        private void JumpTable(int CountInputI)
+        private void TitleJumpTable(int CountInputI, int CountOutputI, int CountCycleI, int CountJumpI, Table table)
         {
-            // Create Table
-            Table table = new Table();
             // Add Table to Doc
             document.Blocks.Add(table);
             // Properties Table
             table.CellSpacing = 10;
             table.Background = Brushes.White;
+            table.BorderBrush = Brushes.Black;
+            ThicknessConverter thicknessConverter = new ThicknessConverter();
+            table.BorderThickness = (Thickness)thicknessConverter.ConvertFromString("1");
             // Create Column Table
-            for (int i = 0; i < CountInputI; i++)
+            for (int i = 0; i <= Math.Pow(2, CountInputI); i++)
             {
                 table.Columns.Add(new TableColumn());
-                if (i % 2 == 0)
-                    table.Columns[i].Background = Brushes.Beige;
-                else
-                    table.Columns[i].Background = Brushes.LightSteelBlue;
+                if (i > 0)
+                {
+                    if (i % 2 == 0)
+                        table.Columns[i].Background = Brushes.LightBlue;
+                    else
+                        table.Columns[i].Background = Brushes.LightCoral;
+                }
+                
             }
             // Create and add an empty TableRowGroup to hold the table's Rows.
             table.RowGroups.Add(new TableRowGroup());
@@ -80,7 +88,7 @@ namespace JumpTableCreate
             // Add the header row with content, 
             currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Таблица переходов и выходов"))));
             // and set the row to span all columns.
-            currentRow.Cells[0].ColumnSpan = CountInputI;
+            currentRow.Cells[0].ColumnSpan = Convert.ToInt32(Math.Pow(2, CountInputI));
             // Add the second (header) row.
             table.RowGroups[0].Rows.Add(new TableRow());
             currentRow = table.RowGroups[0].Rows[1];
@@ -89,11 +97,30 @@ namespace JumpTableCreate
             currentRow.FontWeight = FontWeights.Bold;
             // Add cells with content to the second row.
             currentRow.Cells.Add(new TableCell(new Paragraph(new Run("S"))));
-            for (int i = 0; i < CountInputI; i++)
+            for (int i = 0b_00; i < Math.Pow(2,CountInputI); i++)
             {
-                currentRow.Cells.Add(new TableCell(new Paragraph(new Run("X"+ i))));
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(Convert.ToString(i, toBase: 2)))));
             }
-
+            for (int i = 1; i <= CountJumpI; i++)
+            {
+                table.RowGroups[0].Rows.Add(new TableRow());
+                currentRow = table.RowGroups[0].Rows[i + 1];
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(i.ToString()))));
+            }
+        }
+        /// <summary>
+        /// Создание содержимого таблицы
+        /// </summary>
+        private void CreateTable(int CountInputI, int CountOutputI, int CountCycleI, int CountJumpI, Table table)
+        {
+            for (int i = 1; i <= CountJumpI; i++)
+            {
+                TableRow currentRow = table.RowGroups[0].Rows[i + 1];
+                for (int j = 1; j <= Math.Pow(2, CountInputI); j++)
+                {
+                    currentRow.Cells.Add(new TableCell(new Paragraph(new Run(i.ToString() + j.ToString()))));
+                }
+            }
         }
 
         /// <summary>
